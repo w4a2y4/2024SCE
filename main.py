e95b83e93345572e4bc96f4f086fdcdfc91d85f2
CharlotteNing <w4a2y4@gmail.com>
2024-03-17 17:22:59 +0100

    ask gpt about book

--- main.py
+++ main.py
@@ -3,29 +3,44 @@ from sic_framework.devices.common_naoqi.naoqi_text_to_speech import NaoqiTextToS
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
+import sys
+from openai import OpenAI
+
 from transcript import *
 from gui_components import *
-import sys
 from ch1 import ch1
 from ch2 import ch2
 
 ROBOT_EXIST = False
 
-class NaoController():
-    def __init__(self, nao):
+class Controller():
+    def __init__(self, nao, gpt_client):
         self.nao = nao
+        self.gpt_client = gpt_client
 
     def tts(self, text):
         if self.nao != None:
             self.nao.tts.request(NaoqiTextToSpeechRequest(text, pitch_shift=1.0, volume=1.5, speed=50), block=False)
-        print(f"**SPEAKING** {text}")
+        print(f"** SPEAKING ** {text}")
+        return
+    
+    def askGpt(self, prompt):
+        response = self.gpt_client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": SYS_PROMPT},
+                {"role": "user", "content": prompt}
+            ]
+        )
+        print(f"** GPT Response ** {response}")
+        self.tts(response.choices[0].message.content)
         return
 
 class MainWindow(QMainWindow):
-    def __init__(self, tts):
+    def __init__(self, controller):
         super().__init__()
 
-        self.tts = tts
+        self.controller = controller
 
         layout = QHBoxLayout()
         layout.addLayout(self._greetingsLayout())
@@ -38,6 +53,7 @@ class MainWindow(QMainWindow):
         root.addLayout(layout)
         root.addWidget(HorizontalSeperatorLine())
         root.addLayout(self._freeInputLayout())
+        root.addLayout(self._askGPTLayout())
 
         widget = QWidget()
         widget.setLayout(root)
@@ -104,9 +120,20 @@ class MainWindow(QMainWindow):
         self.textbox.move(20, 20)
         self.textbox.resize(280,40)
         layout.addWidget(self.textbox)
-        go_btn = QPushButton("GO")
+        go_btn = QPushButton("SAY IT")
         go_btn.clicked.connect(self.sendText)
         layout.addWidget(go_btn)
+        return layout
+    
+    def _askGPTLayout(self):
+        layout = QHBoxLayout()
+        self.gptBox = QLineEdit(self)
+        self.gptBox.move(20, 20)
+        self.gptBox.resize(280,40)
+        layout.addWidget(self.gptBox)
+        ask_btn = QPushButton("ASK GPT")
+        ask_btn.clicked.connect(self.askGpt)
+        layout.addWidget(ask_btn)
         return layout
 
     def setBtn(self, kvp: tuple):
@@ -119,20 +146,28 @@ class MainWindow(QMainWindow):
     def onClick(self):
         widget = self.sender()
         msg = widget.property("msg")
-        self.tts(msg)
+        self.controller.tts(msg)
 
     def sendText(self):
-        self.tts(self.textbox.text())
+        self.controller.tts(self.textbox.text())
         self.textbox.setText("")
+    
+    def askGpt(self):
+        self.controller.askGpt(self.gptBox.text())
+        self.gptBox.setText("")
 
 
 if __name__ == "__main__":
-    # connect to robot
-    controller = NaoController(Nao(ip='192.168.0.151') if ROBOT_EXIST else None)
+
+    # setup gpt
+    gpt_client = OpenAI()
+
+     # connect to robot
+    controller = Controller(Nao(ip='192.168.0.151') if ROBOT_EXIST else None, gpt_client)
 
     # start gui
     app = QApplication(sys.argv)
-    window = MainWindow(controller.tts)
+    window = MainWindow(controller)
     window.show()
 
     app.exec()
